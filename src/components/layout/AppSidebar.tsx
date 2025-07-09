a13462347fc9d5f2cc96ad9b703219008610bf62
My Two <mytwo150508@gmail.com>
2025-07-09 23:00:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/AppSidebar.tsx
+++ src/components/layout/AppSidebar.tsx
@@ -31,12 +31,12 @@ export function AppSidebar() {
                 <SidebarMenu>
                     {menuItems.map((item) => (
                         <SidebarMenuItem key={item.href}>
-                            <Link href={item.href} legacyBehavior passHref>
-                                <SidebarMenuButton isActive={pathname === item.href}>
+                            <SidebarMenuButton asChild isActive={pathname === item.href}>
+                                <Link href={item.href}>
                                     <item.icon className="h-5 w-5" />
                                     <span>{item.label}</span>
-                                </SidebarMenuButton>
-                            </Link>
+                                </Link>
+                            </SidebarMenuButton>
                         </SidebarMenuItem>
                     ))}
                 </SidebarMenu>
